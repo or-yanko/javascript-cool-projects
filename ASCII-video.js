d2b385f9fec98a1b8d09cac78092628844112b3e
or yanko <or@mytay.co.il>
2022-03-05 17:12:06 +0200

    c

---
+++ ASCII-video.js
@@ -0,0 +1,88 @@
+const density = "Ñ@#W$9876543210?!abc;:+=-,._                    ";
+//const density = '       .:-i|=+%O#@'
+
+let video;
+let asciiDiv;
+
+function setup() {
+  noCanvas();
+  video = createCapture(VIDEO);
+  video.size(64, 48);
+  asciiDiv = createDiv();
+}
+
+function draw() {
+  video.loadPixels();
+  let asciiImage = "";
+  for (let j = 0; j < video.height; j++) {
+    for (let i = 0; i < video.width; i++) {
+      const pixelIndex = (i + j * video.width) * 4;
+      const r = video.pixels[pixelIndex + 0];
+      const g = video.pixels[pixelIndex + 1];
+      const b = video.pixels[pixelIndex + 2];
+      const avg = (r + g + b) / 3;
+      const len = density.length;
+      const charIndex = floor(map(avg, 0, 255, 0, len));
+      const c = density.charAt(charIndex);
+      if (c == " ") asciiImage += "&nbsp;";
+      else asciiImage += c;
+    }
+    asciiImage += '<br/>';
+  }
+  asciiDiv.html(asciiImage);
+}
+
+
+
+
+
+
+
+
+
+/*onst density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+let video;
+let asciiDiv;
+
+function setup(){
+    noCanvas();
+    video = createCapture(VIDEO);
+    video.size(48,48);
+    asciiDiv = createDiv();
+}
+
+function draw(){
+    video.loadPixels();
+    let asciiImage = '';
+    for (let j=0; j <video.width; j++)
+    {
+        let row = '';
+        for (let i=0; i <video.width; i++)
+        {
+            const pixelIndex = (i + j + video.width) * 4;
+            const r = video.pixels[pixelIndex+0];
+            const g = video.pixels[pixelIndex+1];
+            const b = video.pixels[pixelIndex+2];
+            avg = (r+g+b)/3;
+
+            const len = density.length;
+            const charindex = floor(map(avg, 0,255,len));
+
+            const c = density.charAt(charindex);
+
+            if( c ==" ") asciiImage += "&nbsp;";
+            else asciiImage += c;
+        }
+        asciiImage += "<br/>";
+    }
+    asciiDiv.html(asciiImage);
+
+}
+
+
+
+console.log(1);
+setup();
+while (true){
+    draw();
+}*/
